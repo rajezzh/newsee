0d7b0f48192e1d13730ae0ce58cc8f2e40092deb
KarthickTechie <reachkarthickprogramer@gmail.com>
2025-05-19 16:22:52 +0530

    feat : leadsave bloc added

---
+++ lib/feature/savelead/bloc/savelead_event.dart
@@ -0,0 +1,15 @@
+/* 
+
+encapsulate functionalites for 
+
+0. init - LeadInit
+
+1.showing loadig when lead save button is clicked - loading event
+
+2.after 10s dismiss loading - success | failure 
+
+3.return a new lead object with id as response // LeadData
+
+4. show completed icon in the tab bar // 
+
+ */
