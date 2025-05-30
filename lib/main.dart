552213a3e474f7d47899525138cb5cf5d29979e1
KarthickTechie <reachkarthickprogramer@gmail.com>
2025-05-30 19:13:14 +0530

    refactor: removed ununsed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:newsee/AppSamples/RouterApp/routerapp.dart';
-import 'package:newsee/AppSamples/ToolBarWidget/toolbar.dart';
-import 'package:newsee/Utils/injectiondependency.dart';
-import 'package:newsee/app.dart';
-import 'package:newsee/widgets/counter.dart';
-import 'package:newsee/widgets/news.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   // runApp(MyApp()) // Default MyApp()
