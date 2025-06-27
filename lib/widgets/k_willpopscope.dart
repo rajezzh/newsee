f4cfae725486b7b5f0f52f5b3532ef7adc031bdc
KarthickTechie <reachkarthickprogramer@gmail.com>
2025-06-27 13:55:35 +0530

    feat : widget kwillpopscope developed to handle custom route capability

---
+++ lib/widgets/k_willpopscope.dart
@@ -0,0 +1,63 @@
+/* 
+@author   :  @vishnucenddle [https://github.com/vishnucenddle] 25/06/2025
+@desc     :  interview task given to solve problem in custom navigation in pages
+ */
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+class Kwillpopscope extends StatefulWidget {
+  final FormGroup form;
+  final Widget widget;
+  final BuildContext routeContext;
+  Kwillpopscope({
+    required this.routeContext,
+    required this.form,
+    required this.widget,
+  });
+
+  @override
+  State<Kwillpopscope> createState() => _KwillpopscopeState();
+}
+
+class _KwillpopscopeState extends State<Kwillpopscope> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        bool hasValues = widget.form.controls.values.any(
+          (control) => control.value != null && control.value != '',
+        );
+
+        if (hasValues) {
+          return await showDialog(
+                context: context,
+                builder:
+                    (context) => AlertDialog(
+                      title: Text('Unsaved Changes'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(false);
+                          },
+                          child: Text('Cancel'),
+                        ),
+
+                        TextButton(
+                          onPressed: () {
+                            widget.routeContext.goNamed('home');
+                          },
+                          child: Text('Yes'),
+                        ),
+                      ],
+                    ),
+              ) ??
+              false;
+        }
+        return true;
+      },
+      child: widget.widget,
+    );
+  }
+}
